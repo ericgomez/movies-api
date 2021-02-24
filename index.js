9124e93d199e36d0a03b9a1120083d0f7f4acfac
Eric Gomez <eric.gomez.sanchez@gmail.com>
2021-02-24 12:12:21 -0600

    Implementando un crud en Express.js

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const express = require('express');
 const app = express();
 
 const { config } = require('./config/index');
+const moviesApi = require('./routes/movies.js');
 
-app.get('/', function(request, response) {
-  response.send('Hello World');
-});
-
-
-app.get('/json', function(request, response) {
-  response.json({ hello: 'world'});
-});
+moviesApi(app);
 
 app.listen(config.port, function() {
   console.log(`listening address http://localhost:${config.port}`);
